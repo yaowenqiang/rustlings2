47701933fa4e78156e3519f2442a6b81cd97a3ed
yaowenqiang <yaowenqiang111@163.com>
2025-08-22 23:57:51 +0800

    finish 72

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -11,21 +11,33 @@ enum DivisionError {
 // TODO: Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
-    todo!();
+    if b == 0 {
+        Err(DivisionError::DivideByZero)
+    } else if a == i64::MIN && b == -1 {
+        Err(DivisionError::IntegerOverflow)
+    } else if a % b == 0 {
+        return Ok(a / b);
+    } else {
+        Err(DivisionError::NotDivisible)
+    }
 }
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `Ok([1, 11, 1426, 3])`
-fn result_with_list() {
+fn result_with_list() -> Result<[i64; 4], DivisionError> {
     let numbers = [27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let mut results = [0; 4];
+    for (i, &n) in numbers.iter().enumerate() {
+        results[i] = divide(n, 27)?;
+    }
+    Ok(results)
 }
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `[Ok(1), Ok(11), Ok(1426), Ok(3)]`
-fn list_of_results() {
+fn list_of_results() -> [Result<i64, DivisionError>; 4] {
     let numbers = [27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    numbers.map(|n| divide(n, 27))
 }
 
 fn main() {
