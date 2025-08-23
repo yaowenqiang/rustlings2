f5518ec8292a7f955df58d1573c6294b123dd5b6
yaowenqiang <yaowenqiang111@163.com>
2025-08-23 08:13:43 +0800

    finish 73

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -10,6 +10,11 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+    if num <= 1 {
+        1
+    } else {
+        factorial(num - 1) * num
+    }
 }
 
 fn main() {
